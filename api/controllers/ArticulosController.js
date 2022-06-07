37a18163741d6b7e89d21c8209ef6a57910dc2c9
FIDEL DE LA ROCHA <chindacuil.45@gmail.com>
2022-06-06 23:01:48 -0500

    funcion de usuarios y articulos

---
+++ api/controllers/ArticulosController.js
@@ -0,0 +1,20 @@
+/**
+ * ArticulosController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+    getAllUsers:async function (req,res)
+    {
+        var misArticulos=await User.find();
+        res.ok(misArticulos);
+
+    }
+
+
+};
+
---
+++ api/controllers/UsuariosController.js
@@ -0,0 +1,19 @@
+/**
+ * UsuariosController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+    getAllUsers:async function (req,res)
+    {
+        var misUsuarios=await User.find();
+        res.ok(misUsuarios);
+
+    }
+  
+
+};
+
